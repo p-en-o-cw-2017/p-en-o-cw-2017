ab6a2dc922907be6888bc39e480dd562b302004d
Bart Jacobs <bart.jacobs@cs.kuleuven.be>
2017-10-20 15:58:22 +0200

    Create Autopilot_v2.java

---
+++ Autopilot_v2.java
@@ -0,0 +1,7 @@
+package p_en_o_cw_2017;
+
+public interface Autopilot_v2 {
+    AutopilotOutputs simulationStarted(AutopilotConfig config, AutopilotInputs_v2 inputs);
+    AutopilotOutputs timePassed(AutopilotInputs_v2 inputs);
+    void simulationEnded();
+}
